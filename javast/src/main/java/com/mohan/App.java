8551b8c3e32f367392f2d0160ef32a0e52bb1863
yannampallivamsi1998 <yannampallivamsi1998@gmail.com>
2022-10-12 11:05:15 +0530

    added new file

---
+++ javast/src/main/java/com/mohan/App.java
@@ -0,0 +1,13 @@
+package com.mohan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
